09330ba00618aa09b592b323f1e1b942445e6df2
jeremysr <jeremysr@protonmail.com>
2023-09-17 12:35:32 -0600

    added functionality to generate a badge to add to future readmes

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 //packages and modules required for application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const {renderPackageVersion} = require("./utils/badgeMaker");
 const fs = require("fs");
 //any global variables we may need
 let fileName = "README.md";
+
+//choose an option, generate readme or generate a badge for readme
+const landing = {
+    first:{
+        type:"list",
+        name:"landingPrompt",
+        message:"What would you like to do?",
+        choices:[
+        "Generate a readme",
+        "Generate a badge for a readme"
+    ]
+    },
+    second:[
+        {
+            type:"input",
+            name:"package",
+            message:"Type in the package name:"
+        },
+        {
+            type:"input",
+            name:"version",
+            message:"Type in the package version:"  
+        }
+    ],
+    third:{
+        type:"list",
+        name:"exit",
+        message:"what to do next?",
+        choices:[
+            "start over",
+            "exit"
+        ]
+    }
+};
 
 //An array of questions to ask the user with inquirer.prompt
 const questions = [
@@ -75,14 +110,26 @@ const questions = [
 ];
 
 //a function that will write the readme file to /generated_files/
-function writeToFile(fileName, data) { 
-    fs.writeFile(`./generated_files/${fileName}`, data, function () {
-        console.log(`SUCCESS! README written to ./generated_files/${fileName}`);
-    })
+async function writeToFile(fileName, data) { 
+    fs.writeFile(`./generated_files/${fileName}`, data, async function () {
+        console.log(`SUCCESS! File written to ./generated_files/${fileName}`);
+    });
 }
 
-//application initialization
-function init() {
+//application initialization to landing prompt
+async function init(){
+    const landingPath = await inquirer.prompt(landing.first);
+    (landingPath.landingPrompt === landing.first.choices[0]) ? init2() 
+    : inquirer.prompt(landing.second).then(async (answers)=>{
+        const badge = await renderPackageVersion(answers.package, answers.version);
+        await writeToFile(`${answers.package}-${answers.version}.svg`, badge);
+        const nextAction = await inquirer.prompt(landing.third);
+        (nextAction === landing.third.choices[0]) ? init() : process.exit();
+    });
+}
+
+//application initialization to main readme generating prompt
+function init2() {
     inquirer.prompt(questions).then((answers)=> {
         //pass the data from users answers to inquire.prompt, to generateMarkdown, then write the readme file to /generated_files/
         console.log(answers);
